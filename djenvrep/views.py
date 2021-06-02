b2b135ff95d26a0ebe0a29ffd164871398f5faaf
armands@e-cilveks.lv <armands@e-cilveks.lv>
2021-06-02 15:50:07 +0300

    update django

--- djenvrep/views.py
+++ djenvrep/views.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
 """Django Environment Report."""
-# Future
-from __future__ import absolute_import
-from __future__ import unicode_literals
 
 # Django
 from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render
-from django.views.debug import get_safe_settings
+from django.views.debug import SafeExceptionReporterFilter
 
 # Local
 from .utils import get_db_info
@@ -18,23 +15,25 @@ from .utils import get_python_paths
 from .utils import get_system_locales
 from .utils import get_versions
 
+get_safe_settings = SafeExceptionReporterFilter().get_safe_settings()
+
 
 @user_passes_test(lambda u: u.is_superuser)
-def environment_settings_view(request, template_name='environment-report.html'):
+def environment_settings_view(request, template_name="environment-report.html"):
     """
     Generate a report of the django environment.
 
     Only Superusers have access to it.
     """
     context = {
-        'settings': get_safe_settings(),
-        'installed_packages': get_installed_packages(),
-        'versions': get_versions(),
-        'python_paths': get_python_paths(),
-        'db': get_db_info(),
-        'migrations': get_migrations(),
-        'locales': get_system_locales(),
-        'locale_info': get_locale_info(),
+        "settings": get_safe_settings(),
+        "installed_packages": get_installed_packages(),
+        "versions": get_versions(),
+        "python_paths": get_python_paths(),
+        "db": get_db_info(),
+        "migrations": get_migrations(),
+        "locales": get_system_locales(),
+        "locale_info": get_locale_info(),
     }
 
     return render(request, template_name, context)
--- manage.py
+++ manage.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
+# Standard Library
 import os
 import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testproj.settings")
     try:
+        # Django
         from django.core.management import execute_from_command_line
     except ImportError:
         # The above import may fail for some other reason. Ensure that the
         # issue is really that Django is missing to avoid masking other
         # exceptions on Python 2.
         try:
+            # Django
             import django
         except ImportError:
             raise ImportError(
